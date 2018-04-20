bee128c80a51c1352dd93097e8bd537bc5db7fcd
SimonPreisiner <simonpreisinger@live.de>
2018-04-20 07:46:00 +0200

    20.4 comparable

--- src/Tribble.java
+++ src/Tribble.java
@@ -1,6 +1,10 @@
-public class Tribble {
+public class Tribble implements Comparable<Tribble>{
     private final int size;
+
     private final int happiness;
+
+    private static int nextNumber = 1;
+    private final int number = nextNumber++;
 
     public Tribble(int size, int happiness) {
         this.size = size;
@@ -24,15 +28,46 @@ public class Tribble {
         //return 0;
     }
 
+    @Override
+    public int compareTo(Tribble other) {
+        //equal und hashSet müssen auch angepasst werden sonst unterschiedliche
+        // Ergebnisse
+        if(number < other.number)
+            return 1;
+        if(number > other.number)
+            return -1;
+        return 0;
+        /*
+        System.out.printf(this + "Tribble{%s} compare.To other{%s}\n", size, other);
+        if(size < other.size)
+            return -238; // Zahl egal
+        if(size > other.size)
+            return 42; // Zahl egal
+
+        // happiness auch vergleichen // man muss sich an equals halten: equals sollte gleiches Ergebnis haben
+        if(happiness < other.happiness)
+            return -1;
+        if(happiness > other.happiness)
+            return Integer.MAX_VALUE;
+
+        return 0;
+        */
+    }
 
     @Override
     public boolean equals(Object obj) {
-        // ...
 
-       // if(this.size != other.size)
-       //     return false;
-       // if(this.happiness != other.happiness)
-       //     return false;
+        if(this == obj)
+            return true;
+        if(obj == null) return false;
+        if(getClass() != obj.getClass())
+            return false;
+        final Tribble other = (Tribble)obj;
+        if(this.size != other.size)
+            return false;
+        if(this.happiness != other.happiness)
+            return false;
+
        return true;
     }
 
--- src/TribbleMain.java
+++ src/TribbleMain.java
@@ -2,6 +2,23 @@ import java.util.*;
 
 public class TribbleMain {
     public static void main(String... args) {
+        // Set<Tribble> tribbles = new TreeSet<>();
+        List<Tribble> tribbles = new ArrayList<>();
+        tribbles.add(new Tribble(1,100));
+        tribbles.add(new Tribble(15,50));
+        tribbles.add(new Tribble(15,100));
+        System.out.println(tribbles);
+        Collections.sort(tribbles); // hält sich an meine sort methode
+        System.out.println(tribbles);
+
+        List<Tribble> fixedTribbles = Collections.unmodifiableList(tribbles);
+        System.out.println(fixedTribbles.size());
+        fixedTribbles.remove(new Tribble(15,50)); // schützt nicht inhalt, aber Collectoin drum Fehler
+
+    }
+
+
+        /*
         List<Tribble> tlist = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             tlist.add(new Tribble(i, i));
@@ -39,6 +56,7 @@ public class TribbleMain {
             System.out.println(t);
         }
     }
+    */
     //
     //Set<Tribble> tribbles = new HashSet<>();
 
